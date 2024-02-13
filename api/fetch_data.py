4fa72e5b538871c7bcee3fb24c7b4b494c647415
Caitlin Pratt <cpratt@swingleft.org>
2024-02-13 15:59:45 -0600

    API call running

--- api/fetch_data.py
+++ api/fetch_data.py
@@ -1,15 +1,22 @@
 import requests
 import json
+import time
 import pandas as pd
 import os.path
-import time
+
+
+# TODO: Handle creating/overwriting files
+# TODO: Handle converting JSON to readable/searchable type
+# TODO: Examine data
+# TODO: Implement API call class function?
+
 
 API_URL = 'https://www.clinicaltrials.gov/api/v2/studies'
 
 def make_trials_api_call(
         fields=['BaselineMeasure', 'StatusModule', 'ConditionSearch', 
                 'InterventionType'], limit=10,
-                         next_page = False) -> {json, str}:
+                         pageToken = None) -> {json, str}:
     """
     Makes an API call to NIH's clinical trials API.
 
@@ -22,15 +29,16 @@ def make_trials_api_call(
     """
 
     FIELDS = '|'.join(fields)
-    pageToken = None
     payload = {'fields': FIELDS, 'query.intr': 'DRUG', 'pageToken':pageToken}
     
     r = requests.get(
         'https://www.clinicaltrials.gov/api/v2/studies', params=payload)
+    
+    r.raise_for_status()
 
     return r
 
-def write_data(data, filename, append=True):
+def write_data(data, source, append=True):
     """
     Writes data returned by an API call to a JSON file format.
     
@@ -48,17 +56,26 @@ def write_data(data, filename, append=True):
     else:
         mode = 'w'
     
+    filename = os.path.join('./data', source + '.json')
     with open(filename, mode=mode) as f:
             f.write(json.dumps(data))
 
-def pull_trials_data():
+def pull_trials_data(limit):
     """
     Pulls trials data, and writes it to a JSON file.
     """
-    pass 
+    count_results = 0
+    print(f"Making initial API call for records between 0 and {limit}")
+    results = make_trials_api_call().json()
+    count_results += len(results)
+    next_page_token = results['nextPageToken']
 
-##TODO: Handle waits, next page token
-##TODO: Handle adding to json file, not overrwriting
-#nextPageToken = ''
-#while nextPageToken:
+    count = 0
+    while next_page_token and count < 3:
+        time.sleep(2)
+        count += 1
+        print('Making API call')
+        results = make_trials_api_call(
+             limit=limit, pageToken=next_page_token).json()
+        write_data(results, 'trials')
 
